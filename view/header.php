eda5b3ae8c994e7e530353e05d4aa986f59f12d8
RedaMeziouni <meziounidb@gmail.com>
2021-05-16 14:06:25 +0100

    View Folder

--- view/header.php
+++ view/header.php
@@ -1,3 +1,3 @@
 <header class="header">
-    <h1> <a href="index.php"> Multiplication <br class="title-break"> Cards </a> </h1>
+    <h1> <a href="index.php"> Multiplication Flash<br class="title-break"> Cards </a> </h1>
 </header>
