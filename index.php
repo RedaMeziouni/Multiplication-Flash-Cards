a1d9049447d7a7c522e7262ef0aea2d82a0c63be
RedaMeziouni <meziounidb@gmail.com>
2021-05-16 14:03:05 +0100

    INDEX

--- index.php
+++ index.php
@@ -8,10 +8,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./sass/main.css">
     <title>Flash Cards</title>
 </head>
 <body>
+    <!-- Include header -->
     <?php include("./view/header.php"); ?>
+    <!-- Structure -->
+    <?php
+        if ($number) {
+            include("./view/results.php");
+        } else {
+            include("./view/form.php");
+        }
+    ?>
+    <!-- include footer -->
     <?php include("./view/footer.php"); ?>
 </body>
 </html>
