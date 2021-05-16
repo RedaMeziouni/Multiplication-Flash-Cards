c64b2cc5cb7f9403e3b59638d38e5732275c7da1
RedaMeziouni <meziounidb@gmail.com>
2021-05-16 13:11:45 +0100

    Adding the br x class

--- view/header.php
+++ view/header.php
@@ -1,3 +1,3 @@
-<header>
-    <h1><a href="index.php">Multiplication Cards</a></h1>
+<header class="header">
+    <h1> <a href="index.php"> Multiplication <br class="title-break"> Cards </a> </h1>
 </header>
